9fe90f9dccdf0ada9d16b058f98888a22e15d1e0
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-20 19:26:08 +0100

    Add Zabbix agent

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/zabbix/zabbix-server-pgsql:6.0-alpine-latest docker.io/zabbix/zabbix-web-nginx-pgsql:6.0-alpine-latest" \
+    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/zabbix/zabbix-server-pgsql:6.0-alpine-latest docker.io/zabbix/zabbix-web-nginx-pgsql:6.0-alpine-latest docker.io/zabbix/zabbix-agent2:6.0-alpine-latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
