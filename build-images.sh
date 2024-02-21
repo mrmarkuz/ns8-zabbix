5a647b70d633019fa71aa63a6bdeecc48a17de5c
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-21 22:20:20 +0100

    Change traefik auth from node to any

--- build-images.sh
+++ build-images.sh
@@ -42,7 +42,7 @@ buildah add "${container}" ui/dist /ui
 # rootfull=0 === rootless container
 # tcp-ports-demand=1 number of tcp Port to reserve , 1 is the minimum, can be udp or tcp
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=node:fwadm traefik@node:routeadm" \
+    --label="org.nethserver.authorizations=node:fwadm traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/zabbix/zabbix-server-pgsql:6.0-alpine-latest docker.io/zabbix/zabbix-agent2:6.0-alpine-latest docker.io/zabbix/zabbix-web-nginx-pgsql:6.0-alpine-latest" \
