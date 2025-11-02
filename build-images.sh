8834533b689b2f0fc111e930c1ece86ac960f3bc
mrmarkuz <mrmarkuz666@gmail.com>
2025-11-02 20:19:05 +0100

    Allow one instance per node only

--- build-images.sh
+++ build-images.sh
@@ -44,6 +44,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=node:fwadm traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
+	--label="org.nethserver.max-per-node=1" \
 	--label="org.nethserver.min-core=3.12.4-0" \
     --label="org.nethserver.images=docker.io/postgres:15.14-alpine docker.io/zabbix/zabbix-server-pgsql:7.0.20-alpine docker.io/zabbix/zabbix-agent2:7.0.20-alpine docker.io/zabbix/zabbix-web-nginx-pgsql:7.0.20-alpine" \
     "${container}"
